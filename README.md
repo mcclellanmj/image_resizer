501fcdd68989f31d64e4fbe77b9cecec63610b9b
Matt McClellan <mcclellan.mj@gmail.com>
2014-07-20 22:09:31 -0500

    - Changed behavior to act more like the copy (cp) command in Linux - Added wildcard expansion for windows - Updated documentation to match changes - Fixed typo in documentation - Added documentation to script

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
 Image Resizer
 -------------
-Tool to resize an image or list of images and save them back to their source.  Will save any exif data that is on the image.
+Tool to resize an image or list of images and save them back to their source.  It will work on anything that PIL can handle.  In the case of JPEGs it will save any exif data that is on the image.
 
 Usage
 -----
-    python -W 1024 -H 1024 main.py image.jpg image2.jpg
+    python -W 1024 -H 1024 main.py image.jpg image2.jpg image3.jpg output
 
 -W signifies the maximum allowable width, this flag is optional and will default to 2048
 
--H siginifies the maximum allowable height, this flag is optional and will default to 2048
+-H signifies the maximum allowable height, this flag is optional and will default to 2048
+
+In the case that multiple files are passed in the output location must be an existing directory.  If only one file to be resized
+is passed in the output can be a file or directory.  If it is a directory the resized file will be placed in the directory, if
+it does not exist then the file will be created.
+
+The usage is intended to mimic the copy (cp) command in linux.  On a Windows machine it will handle the glob expansion that is done automatically on Linux, so for instance 
+
+    python main.py *.jpg resized
+Will resize all files ending with .jpg and output them to the resized directory.  On linux the glob expansion of *.jpg will be automatic, and on Windows the script will handle it.
 
 Installation
 ------------
@@ -17,8 +26,5 @@ Requires Python, should work on 2 or 3 but is only tested on 3
 Requires the Pillow library.
 
 Easy requirement install
-    pip install -r requirements.txt
 
-Warning
--------
-This will overwrite the existing image with the resized image (by intention).  Make a copy of your originals if you want to keep them at that size. 
+    pip install -r requirements.txt
--- main.py
+++ main.py
@@ -1,19 +1,76 @@
 import argparse
 from PIL import Image
+import os
+from glob import glob
+
+def single_mode(output):
+    """
+    This function is to generate another function which can be used for generating file names
+    :param output:  the output location the user has passed in
+    :return: A function which will generate a filename based on the output parameter
+    """
+    def create_path(current_file):
+        if os.path.isdir(output):
+            return os.path.join(output, current_file)
+        else:
+            return output
+
+    return create_path
+
+def multi_mode(output):
+    """
+    This function is to generate another function which can be used for generating file names
+    :param output: The output location the user entered
+    :return: A function which will create paths for a file based on the output directory
+    """
+    if not os.path.isdir(output):
+        raise Exception("When passing in multiple files the destination must be an existing directory")
+
+    def create_path(current_file):
+        return os.path.join(output, current_file)
+
+    return create_path
+
+def expand_for_windows(files):
+    """
+    Do works that Windows should be doing for us, expand out the CLI wildcards
+    :param files: list of files based in from Windows CLI
+    :return: expanded list if any needed expansion, this function will always return 1 or more, if not we error out
+    """
+    expanded_files = []
+    for file in files:
+        expanded_files.extend(glob(file))
+
+    if len(expanded_files) == 0:
+        raise Exception("Not enough files were found, command line argument was [%s]", " ".join(files))
+
+    return expanded_files
+
 
 if __name__ == "__main__":
     argument_parser = argparse.ArgumentParser()
 
-    argument_parser.add_argument('files', metavar='file', default='.', nargs='+')
+    argument_parser.add_argument('files', metavar='file', nargs='+')
+    argument_parser.add_argument('destination', metavar='file', nargs=1)
     argument_parser.add_argument('-W', '--maxwidth', type=int, default=2048, help='Maximum width')
     argument_parser.add_argument('-H', '--maxheight', type=int, default=2048, help='Maximum height')
 
     args = argument_parser.parse_args()
     size = (args.maxwidth, args.maxheight)
+    destination = args.destination[0]
 
-    for file in args.files:
+    # Windows shell doesn't expand because.. well windows so we have to do it for them.  Reimplementing
+    # stuff your OS should do for you is fun.
+    files = args.files if not os.name == 'nt' else expand_for_windows(args.files)
+
+    if len(files) == 1:
+        generate_output_name = single_mode(destination)
+    else:
+        generate_output_name = multi_mode(destination)
+
+    for file in files:
         image = Image.open(file)
         image.thumbnail(size, Image.ANTIALIAS)
         exif = image.info['exif']
-        image.save(file, exif=exif)
+        image.save(generate_output_name(file), exif=exif)
 
